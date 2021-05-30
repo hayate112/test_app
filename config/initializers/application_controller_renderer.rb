7279e5344e9ec9e632772d045b3d8cc17cd82ae8
hayate112 <k.hayate0112@gmail.com>
2021-05-30 18:03:13 +0000

    [Update]画像投稿機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f43f29cf993196bf30847db1aa9a12573d30866dc1bbf9905439b074825761e5f0b1d753b8f1a45e8dcf075641f2a5d5832f830c8515e3e245f5ba0a2d081ae9'
